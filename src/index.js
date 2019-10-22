26ec139efe8d52cd3613c15d7c018f2c401461b0
Ryan Regalado <ryanreg48@gmail.com>
2019-10-21 22:53:35 -0700

    styles: adding skeleton. configuration: added more attributes

--- src/index.js
+++ src/index.js
@@ -11,11 +11,15 @@ const SETTINGS = {
 };
 
 const ATTRIBUTES = [
-  { name: 'Hair style', description: 'Spikey, long, short, etc', values: ['spikey', 'long', 'short'] },
+  { name: 'Hair style', description: 'Spikey, long, short, etc', values: ['spikey', 'long', 'short', 'bob', 'bun', 'man-bun'] },
   { name: 'Weight', description: '', values: [60, 80, 100, 120, 140, 160, 180] },
   { name: 'Height', description: 'From 3\' and up', values: [36, 48, 60, 72, 88, 102, 123] },
   { name: 'Eye color', description: 'Hazel, dark blue, glowing, etc', values: ['Hazel', 'Dark blue', 'Black', 'Green'] },
-  { name: 'Skin color', description: 'Tan, spotted, albino, etc', values: ['Tan', 'Golden', 'Black', 'White', 'Brown'] },
+  { name: 'Skin color', description: 'Tan, spotted, albino, etc', values: ['Tan', 'Golden', 'Black', 'White', 'Brown', 'Orange', 'Yellow'] },
+  { name: 'Body build', description: '', values: ['Frail', 'Fat', 'Thick', 'Boney', 'Medium', 'Small', 'Large', 'Skinny'] },
+  { name: 'Gender', description: '', values: ['Male', 'Female', 'Androgynous', 'Ambiguous'] },
+  { name: 'Accessories', description: '', values: ['Necklace', 'Hat', 'Watch', 'Ring', 'Earring'] },
+  { name: 'Name', description: '', values: ['Tiberius', 'Aliso', 'Harmony', 'Griggio', 'Eliana', 'Brarben', 'Hauminor'] }
 ];
 
 ReactDOM.render(
