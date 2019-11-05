f6a0bb7af66718b8e081c98673c8758254df9644
Ryan Regalado <ryanreg48@gmail.com>
2019-11-05 14:24:40 -0800

    feat(name-generator): install fakerator and test log

--- src/components/CharacterGenerator.js
+++ src/components/CharacterGenerator.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import CharacterGeneratorHeader from './CharacterGeneratorHeader';
 import AttributeSelector from './AttributeSelector';
+import Fakerator from 'fakerator';
 
 const CharacterGenerator = (props) => {
   const { attributes, settings } = props;
+
+  window.fakerator = Fakerator();
+
+  console.log('fakerator', window.fakerator.names.name());
 
   return (
     <div className="container">
