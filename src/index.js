2a4ee71c32b8f88e3b96d0489847680b2638d234
Ryan Regalado <ryanreg48@gmail.com>
2020-11-08 16:58:55 -0800

    feat(ideasgrid): add more ideas

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CharacterGenerator from './components/CharacterGenerator.js';
 import './index.css';
-import { SETTINGS, ATTRIBUTES, ANATOMY, STYLE, EMOTION } from './config';
+import {
+  SETTINGS,
+  ATTRIBUTES,
+  ANATOMY,
+  STYLE,
+  EMOTION,
+  COLOR,
+  ROLE,
+  ITEM,
+  SETTING,
+} from './config';
 const ideasGrid = [
   { name: 'Anatomy', attributes: ANATOMY },
   { name: 'Style', attributes: STYLE },
   { name: 'Emotion', attributes: EMOTION },
+  { name: 'Color', attributes: COLOR },
+  { name: 'Role', attributes: ROLE },
+  { name: 'Item', attributes: ITEM },
+  { name: 'Setting', attributes: SETTING },
 ];
 
 ReactDOM.render(
