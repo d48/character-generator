13f7c18142cc1152f4a09d04ae70dffee6c1f349
Ryan Regalado <ryanreg48@gmail.com>
2020-11-09 10:46:22 -0800

    test(charactergenerator): add ideasGrid to mocked prop data

--- src/components/__test__/CharacterGenerator.test.js
+++ src/components/__test__/CharacterGenerator.test.js
@@ -5,18 +5,28 @@ import CharacterGenerator from '../CharacterGenerator';
 test('Loads Character Generator with props', () => {
   // arrange
   let props = {
-    attributes: [{name: 'boom', values: ['boom']}],
+    attributes: [{ name: 'boom', values: ['boom'] }],
     settings: {
       title: 'boom',
       description: 'boom',
-      buttonLabel: 'boom'
-    }
-  }
+      buttonLabel: 'boom',
+    },
+    ideasGrid: [
+      { name: 'anatomy', attributes: ['boom'] },
+      { name: 'anatomy', attributes: ['boom'] },
+    ],
+  };
   const { container } = render(<CharacterGenerator {...props} />);
 
   // act / assert
   expect(container.querySelector('h1')).toHaveTextContent(props.settings.title);
-  expect(container.querySelector('h2')).toHaveTextContent(props.settings.description);
-  expect(container.querySelector('button')).toHaveTextContent(props.settings.buttonLabel);
-  expect(container.querySelectorAll('label')[1]).toHaveTextContent(props.attributes[0].name);
+  expect(container.querySelector('h2')).toHaveTextContent(
+    props.settings.description
+  );
+  expect(container.querySelector('button')).toHaveTextContent(
+    props.settings.buttonLabel
+  );
+  expect(container.querySelectorAll('label')[1]).toHaveTextContent(
+    props.attributes[0].name
+  );
 });
