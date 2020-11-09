041571c1e4f78a2127bdd2aa9bb0d9695a1155bf
Ryan Regalado <ryanreg48@gmail.com>
2020-11-09 01:01:17 -0800

    style(ideasgrid): alignment with flex

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -22,7 +22,7 @@ Tabs.propTypes = {
 };
 
 const Tab = (props) => {
-  const { children, activeTab, ACTIVETAB, id } = props;
+  const { children, activeTab, id } = props;
   return (
     <section className={activeTab === id ? styles.tabActive : styles.tab}>
       <div className={`${styles.content} ${styles.border}`}>{children}</div>
@@ -39,7 +39,7 @@ Tab.propTypes = {
 };
 
 const TabHeader = (props) => {
-  const { children, id, activeTab, ACTIVETAB } = props;
+  const { children, activeTab } = props;
   return (
     <section className={styles.tabHeaderContainer}>
       {children.map((child, key) => {
