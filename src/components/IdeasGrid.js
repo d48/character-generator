331844dd856af205f0b92af5e29dfdb44f87e07f
Ryan Regalado <ryanreg48@gmail.com>
2020-11-15 00:06:54 -0800

    feat(color): add color to IdeasGrid

--- src/components/IdeasGrid.js
+++ src/components/IdeasGrid.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import styles from './IdeasGrid.module.css';
+import { colorShade } from './helpers';
+
+const COLORS = [
+  { PURPLE: '#800080' },
+  { BLUE: '#0000ff' },
+  { RED: '#ff0000' },
+  { ORANGE: '#ffa500' },
+  { YELLOW: '#ffff00' },
+  { YELLOWGREEN: '#9acd32' },
+  { GREEN: '#008000' },
+];
 
 const IdeasGrid = (props) => {
   const { attributes } = props;
   return attributes.map((attribute, index) => {
     return (
-      <div key={index}>
-        <h3>{attribute.name}</h3>
+      <div key={index} className={styles.ideaTopic}>
+        <h3
+          className={styles.header}
+          style={{
+            backgroundColor: colorShade(Object.values(COLORS[index])[0], 60),
+          }}
+        >
+          {attribute.name}
+        </h3>
         <ul>
           {attribute.attributes.map((attrib, index) => {
-            return <li key={index}>{attrib}</li>;
+            return (
+              <li key={index} className={styles.idea}>
+                {attrib}
+              </li>
+            );
           })}
         </ul>
       </div>
